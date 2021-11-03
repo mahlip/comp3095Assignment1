d3247ec739640b64b8595aef388b58fa7e22b9b9
Mohamed Ahlip <Mohamed.Ahlip@georgebrown.ca>
2021-11-03 13:41:07 -0400

    Commit the completed login and registration portion of assignment1

---
+++ src/main/java/ca/gbc/comp3095/assignment1/model/AppUser.java
@@ -0,0 +1,101 @@
+package ca.gbc.comp3095.assignment1.model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name ="APP_USER", uniqueConstraints = @UniqueConstraint( columnNames = "EMAIL"))
+public class AppUser{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(name="FIRST_NAME")
+    private String firstName;
+
+    @Column(name ="LAST_NAME")
+    private String lastName;
+    @Column(name ="EMAIL", nullable = false, unique = true)
+    private String email;
+    @Column(name ="PASSWORD",nullable = false, length = 64)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "APP_USER_ROLE",
+            joinColumns = @JoinColumn(
+                    name = "USER_ID", referencedColumnName = "USER_ID"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "ROLE_ID", referencedColumnName = "USER_ID"
+            )
+    )
+    private Collection<AppUserRole> roles;
+
+    public AppUser(String firstName,
+                String lastName,
+                String email,
+                String password,
+                Collection<AppUserRole> roles) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public AppUser() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<AppUserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<AppUserRole> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/ca/gbc/comp3095/assignment1/service/AppUserService.java
@@ -0,0 +1,9 @@
+package ca.gbc.comp3095.assignment1.service;
+
+import ca.gbc.comp3095.assignment1.model.AppUser;
+import ca.gbc.comp3095.assignment1.web.datatransfer.AppUserRegistrationDataTransfer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AppUserService extends UserDetailsService {
+    AppUser save(AppUserRegistrationDataTransfer appUserRegistrationDataTransfer);
+}
